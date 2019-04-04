1ae84ac023d95cf56981e98edee4bf6c2675198a
Lynn Garren <garren@fnal.gov>
2019-04-04 12:35:38 -0500

    changes for art 3.02

--- ubraw/BeamData/BeamData_module.cc
+++ ubraw/BeamData/BeamData_module.cc
@@ -16,8 +16,8 @@
 #include "art/Framework/Principal/SubRun.h"
 #include "canvas/Utilities/InputTag.h"
 #include "art/Framework/Services/Registry/ServiceHandle.h"
-#include "art/Framework/Services/Optional/TFileService.h"
-#include "art/Framework/Services/Optional/TFileDirectory.h"
+#include "art_root_io/TFileService.h"
+#include "art_root_io/TFileDirectory.h"
 
 #include "IFDH_service.h"
 
