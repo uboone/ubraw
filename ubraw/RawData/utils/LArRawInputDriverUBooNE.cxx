8d730a585e0518047d7d2f6e92e6d2236287a041
Herbert Greenlee <greenlee@fnal.gov>
2020-05-29 20:28:55 -0500

    Update channel map db url.

--- ubraw/RawData/utils/LArRawInputDriverUBooNE.cxx
+++ ubraw/RawData/utils/LArRawInputDriverUBooNE.cxx
@@ -883,7 +883,7 @@ namespace lris {
 	    data_taking_time = fDataTakingTime;
 
 	  std::ostringstream ostr;
-	  ostr << "http://dbdata0vm.fnal.gov:8188/QE/uboone/query?F=get_map_double_sec&a="
+	  ostr << "https://dbdata0vm.fnal.gov:8444/QE/uboone/query?F=get_map_double_sec&a="
 	       << data_taking_time << "&a=" << fSwizzlingTime;
 	  std::cout << "Fetching channel map from database." << std::endl;
 	  std::cout << "Url = " << ostr.str() << std::endl;
